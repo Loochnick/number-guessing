cb40e9af02651b8a6f2d98f4be98d7e2d65c48a9
Loochnick <loochnik@ukr.net>
2024-09-23 13:42:54 +0200

    generateRandomNumber and getPlayerGuess

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
-console.log('Hey!');
+function generateRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  console.log(randomNumber);
+}
+
+generateRandomNumber();
+
+function getPlayerGuess() {
+  let playerGuess = Number(prompt("Please, enter a whole number between 1 and 100"));
+
+  if (
+      playerGuess < 1 ||
+      playerGuess > 100 ||
+      isNaN(playerGuess) ||
+      !Number.isInteger(playerGuess)
+    ) {
+    playerGuess = undefined;
+    getPlayerGuess();
+  } else {
+    console.log(playerGuess);
+    return playerGuess;
+  }
+}
+
+getPlayerGuess();
