ee5dedfcafc6d2ef1dc38cd02537aae58488c753
DESKTOP-A193QHJ\Asus <aminglbayatpour3@gmail.com>
2024-09-24 20:57:11 +0200

    refactoring part one

--- scripts.js
+++ scripts.js
@@ -1,36 +1,98 @@
-let correctGuesses = 0;
+import {
+  MIN_NUMBER,
+  MAX_NUMBER,
+  INVALID_NUMBER_MESSAGE,
+  LOW_GUESS_MESSAGE,
+  LOW_GUESS_CLOSE_MESSAGE,
+  HIGH_GUESS_MESSAGE,
+  HIGH_GUESS_CLOSE_MESSAGE,
+  CORRECT_GUESS_MESSAGE,
+  GUESS_PROMPT_MESSAGE,
+  CLOSE_RANGE,
+  MAX_ATTEMPTS,
+  MAX_ATTEMPTS_REACHED_MESSAGE,
+  TRY_AGAIN_MESSAGE,
+  THANKS_FOR_PLAYING_MESSAGE,
+  WELCOME_MESSAGE,
+} from "./constants.js";
+import { generateRandomNumber, validatePlayerGuess } from "./utils.js";
 
-function generateRandomNumber() {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  return randomNumber;
-}
+const getPlayerGuess = () => {
+  let playerGuessNumber = 0;
+  let isPlayerGuessValid = false;
 
-/* Some test comment */
-function getPlayerGuess() {
-  let playerGuess = Number(prompt("Please, enter a whole number between 1 and 100"));
+  do {
+    playerGuessNumber = prompt(GUESS_PROMPT_MESSAGE);
 
-  if (
-      playerGuess < 1 ||
-      playerGuess > 100 ||
-      isNaN(playerGuess) ||
-      !Number.isInteger(playerGuess)
-    ) {
-    console.log("Invalid number, try again");
-  } else {
-    console.log("Player Guess:", playerGuess);
+    //validating player guess
+    isPlayerGuessValid = validatePlayerGuess(playerGuessNumber);
 
-    const randomNumber = generateRandomNumber();
-    console.log("Random Number:", randomNumber);
-
-    if (playerGuess === randomNumber) {
-      correctGuesses++;
-      console.log("Correct Guess! Total Correct Guesses:", correctGuesses);
-    } else {
-      console.log("Incorrect guess. Try again.");
+    //alert the player if the guess is not valid
+    if (!isPlayerGuessValid) {
+      alert(INVALID_NUMBER_MESSAGE);
     }
+  } while (!isPlayerGuessValid);
+
+  return Number(playerGuessNumber);
+};
+
+const checkGuess = (playerGuess, correctNumber) => {
+  const guessDifference = Math.abs(playerGuess - correctNumber);
+
+  if (playerGuess === correctNumber) {
+    return CORRECT_GUESS_MESSAGE;
+  } else if (playerGuess < correctNumber) {
+    return guessDifference <= CLOSE_RANGE
+      ? LOW_GUESS_CLOSE_MESSAGE
+      : LOW_GUESS_MESSAGE;
+  } else {
+    return guessDifference <= CLOSE_RANGE
+      ? HIGH_GUESS_CLOSE_MESSAGE
+      : HIGH_GUESS_MESSAGE;
+  }
+};
+
+const playGameRound = (correctNumber) => {
+  let numberOfAttempts = 0;
+  let resultMessage = "";
+
+  // Loop until max attempts or correct guess
+  while (
+    numberOfAttempts < MAX_ATTEMPTS &&
+    resultMessage !== CORRECT_GUESS_MESSAGE
+  ) {
+    const playerGuess = getPlayerGuess();
+    resultMessage = checkGuess(playerGuess, correctNumber);
+    alert(resultMessage);
+
+    // Increment attempts
+    numberOfAttempts++;
   }
 
-  setTimeout(getPlayerGuess, 1000);
-}
+  // Check if the player ran out of attempts
+  if (
+    numberOfAttempts === MAX_ATTEMPTS &&
+    resultMessage !== CORRECT_GUESS_MESSAGE
+  ) {
+    alert(MAX_ATTEMPTS_REACHED_MESSAGE);
+  }
+};
 
-getPlayerGuess();
+const game = () => {
+  const correctNumber = generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
+  let shouldRestart = false;
+  console.log("guess", correctNumber);
+
+  //Welcome Message(only for the first round)
+  !shouldRestart && alert(WELCOME_MESSAGE);
+
+  // Play the round
+  playGameRound(correctNumber);
+
+  //restarting the game
+  shouldRestart = confirm(TRY_AGAIN_MESSAGE);
+  shouldRestart ? game() : alert(THANKS_FOR_PLAYING_MESSAGE);
+};
+
+// play
+game();
