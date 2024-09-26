3a279323125492ef75d6c81a0ef26e653a22a6a0
DESKTOP-A193QHJ\Asus <aminglbayatpour3@gmail.com>
2024-09-26 17:15:49 +0200

    made an improvement regarding handling the constants

--- constants.js
+++ constants.js
@@ -1,41 +1,50 @@
-const MIN_NUMBER = 1;
-const MAX_NUMBER = 100;
-const CLOSE_RANGE = 10;
-const MAX_ATTEMPTS = 10;
+// Game Settings
+export const GAME_SETTINGS = {
+  MIN_NUMBER: 1,
+  MAX_NUMBER: 100,
+  CLOSE_RANGE: 10,
+  MAX_ATTEMPTS: 10,
+};
 
-const GUESS_PROMPT_MESSAGE = `Enter your guess (a number between ${MIN_NUMBER} and ${MAX_NUMBER}).`;
-const INVALID_NUMBER_MESSAGE = `Invalid input. Please enter a number between ${MIN_NUMBER} and ${MAX_NUMBER}.`;
-const MAX_ATTEMPTS_REACHED_MESSAGE = `You've reached the maximum number of ${MAX_ATTEMPTS} attempts. Better luck next time!`;
-const LOW_GUESS_MESSAGE = "Your guess is too low.";
-const LOW_GUESS_CLOSE_MESSAGE = "You're close! Your guess is low.";
-const HIGH_GUESS_MESSAGE = "Your guess is too high.";
-const HIGH_GUESS_CLOSE_MESSAGE = "You're close! Your guess is high.";
-const CORRECT_GUESS_MESSAGE = "Congratulations! You guessed it right!";
-const TRY_AGAIN_MESSAGE = "Would you like to try again?";
-const THANKS_FOR_PLAYING_MESSAGE = "Thanks for playing!";
-const WELCOME_MESSAGE = `
+// Prompt Messages
+export const PROMPTS_MESSAGES = {
+  GUESS_PROMPT: `Enter your guess (a number between ${GAME_SETTINGS.MIN_NUMBER} and ${GAME_SETTINGS.MAX_NUMBER}).`,
+  TRY_AGAIN: "Would you like to try again?",
+};
+
+// Error Messages
+export const ERROR_MESSAGES = {
+  INVALID_NUMBER: `Invalid input. Please enter a number between ${GAME_SETTINGS.MIN_NUMBER} and ${GAME_SETTINGS.MAX_NUMBER}.`,
+  MAX_ATTEMPTS_REACHED: `You've reached the maximum number of ${GAME_SETTINGS.MAX_ATTEMPTS} attempts. Better luck next time!`,
+};
+
+// Feedback Messages
+export const FEEDBACK_MESSAGES = {
+  LOW_GUESS: "Your guess is too low.",
+  LOW_GUESS_CLOSE: "You're close! Your guess is low.",
+  HIGH_GUESS: "Your guess is too high.",
+  HIGH_GUESS_CLOSE: "You're close! Your guess is high.",
+  CORRECT_GUESS: "Congratulations! You guessed it right!",
+};
+
+// Game Flow Messages
+export const GAME_FLOW_MESSAGES = {
+  WELCOME: `
     🎉 Welcome to the Ultimate Number Guessing Game! 🎉
     Here are the rules:
-    - You have up to ${MAX_ATTEMPTS} attempts to guess the correct number.
-    - The number will be between ${MIN_NUMBER} and ${MAX_NUMBER}.
+    - You have up to ${GAME_SETTINGS.MAX_ATTEMPTS} attempts to guess the correct number.
+    - The number will be between ${GAME_SETTINGS.MIN_NUMBER} and ${GAME_SETTINGS.MAX_NUMBER}.
     🔍 Pay close attention to the hints after each guess!
     Can you find the correct number? Good luck, and have fun!
-  `;
+  `,
+  THANKS_FOR_PLAYING: "Thanks for playing!",
+};
 
-export {
-  INVALID_NUMBER_MESSAGE,
-  LOW_GUESS_MESSAGE,
-  LOW_GUESS_CLOSE_MESSAGE,
-  HIGH_GUESS_MESSAGE,
-  HIGH_GUESS_CLOSE_MESSAGE,
-  CORRECT_GUESS_MESSAGE,
-  GUESS_PROMPT_MESSAGE,
-  CLOSE_RANGE,
-  MAX_ATTEMPTS,
-  MAX_ATTEMPTS_REACHED_MESSAGE,
-  MIN_NUMBER,
-  MAX_NUMBER,
-  TRY_AGAIN_MESSAGE,
-  THANKS_FOR_PLAYING_MESSAGE,
-  WELCOME_MESSAGE,
+// Score Messages
+export const SCORE_MESSAGES = {
+  INCREDIBLE: "Incredible! You got it on the first try! Your score is",
+  GREAT_JOB: "Great job! You got it within a few attempts. Your score is",
+  GOOD_JOB: "Good! It took a bit longer, but you still made it. Your score is",
+  TOOK_SEVERAL_ATTEMPTS: "You got it, but it took several attempts. Your score is",
+  BETTER_LUCK_NEXT_TIME: "Better luck next time! Your score is",
 };
--- scripts.js
+++ scripts.js
@@ -1,20 +1,12 @@
 import {
-  MIN_NUMBER,
-  MAX_NUMBER,
-  INVALID_NUMBER_MESSAGE,
-  LOW_GUESS_MESSAGE,
-  LOW_GUESS_CLOSE_MESSAGE,
-  HIGH_GUESS_MESSAGE,
-  HIGH_GUESS_CLOSE_MESSAGE,
-  CORRECT_GUESS_MESSAGE,
-  GUESS_PROMPT_MESSAGE,
-  CLOSE_RANGE,
-  MAX_ATTEMPTS,
-  MAX_ATTEMPTS_REACHED_MESSAGE,
-  TRY_AGAIN_MESSAGE,
-  THANKS_FOR_PLAYING_MESSAGE,
-  WELCOME_MESSAGE,
-} from "./constants.js";
+  GAME_SETTINGS,
+  PROMPTS_MESSAGES,
+  ERROR_MESSAGES,
+  FEEDBACK_MESSAGES,
+  GAME_FLOW_MESSAGES,
+  SCORE_MESSAGES,
+} from "./constants";
+
 import { generateRandomNumber, validatePlayerGuess } from "./utils.js";
 
 const getPlayerGuess = () => {
@@ -22,14 +14,14 @@ const getPlayerGuess = () => {
   let isPlayerGuessValid = false;
 
   do {
-    playerGuessNumber = prompt(GUESS_PROMPT_MESSAGE);
+    playerGuessNumber = prompt(PROMPTS_MESSAGES.GUESS_PROMPT);
 
     //validating player guess
     isPlayerGuessValid = validatePlayerGuess(playerGuessNumber);
 
     //alert the player if the guess is not valid
     if (!isPlayerGuessValid) {
-      alert(INVALID_NUMBER_MESSAGE);
+      alert(ERROR_MESSAGES.INVALID_NUMBER);
     }
   } while (!isPlayerGuessValid);
 
@@ -40,15 +32,15 @@ const checkGuess = (playerGuess, correctNumber) => {
   const guessDifference = Math.abs(playerGuess - correctNumber);
 
   if (playerGuess === correctNumber) {
-    return CORRECT_GUESS_MESSAGE;
+    return FEEDBACK_MESSAGES.CORRECT_GUESS;
   } else if (playerGuess < correctNumber) {
-    return guessDifference <= CLOSE_RANGE
-      ? LOW_GUESS_CLOSE_MESSAGE
-      : LOW_GUESS_MESSAGE;
+    return guessDifference <= GAME_SETTINGS.CLOSE_RANGE
+      ? FEEDBACK_MESSAGES.LOW_GUESS_CLOSE
+      : FEEDBACK_MESSAGES.LOW_GUESS;
   } else {
-    return guessDifference <= CLOSE_RANGE
-      ? HIGH_GUESS_CLOSE_MESSAGE
-      : HIGH_GUESS_MESSAGE;
+    return guessDifference <= GAME_SETTINGS.CLOSE_RANGE
+      ? FEEDBACK_MESSAGES.HIGH_GUESS_CLOSE
+      : FEEDBACK_MESSAGES.HIGH_GUESS;
   }
 };
 
@@ -58,8 +50,8 @@ const playGameRound = (correctNumber) => {
 
   // Loop until max attempts or correct guess
   while (
-    numberOfAttempts < MAX_ATTEMPTS &&
-    resultMessage !== CORRECT_GUESS_MESSAGE
+    numberOfAttempts < GAME_SETTINGS.MAX_ATTEMPTS &&
+    resultMessage !== FEEDBACK_MESSAGES.CORRECT_GUESS
   ) {
     const playerGuess = getPlayerGuess();
     resultMessage = checkGuess(playerGuess, correctNumber);
@@ -71,27 +63,68 @@ const playGameRound = (correctNumber) => {
 
   // Check if the player ran out of attempts
   if (
-    numberOfAttempts === MAX_ATTEMPTS &&
-    resultMessage !== CORRECT_GUESS_MESSAGE
+    numberOfAttempts === GAME_SETTINGS.MAX_ATTEMPTS &&
+    resultMessage !== FEEDBACK_MESSAGES.CORRECT_GUESS
   ) {
-    alert(MAX_ATTEMPTS_REACHED_MESSAGE);
+    alert(ERROR_MESSAGES.MAX_ATTEMPTS_REACHED);
   }
+
+  return numberOfAttempts;
+};
+
+const calculateScore = (numberOfAttempts) => {
+  let score;
+  let message;
+
+  switch (numberOfAttempts) {
+    case 1:
+      score = 60;
+      message = `${SCORE_MESSAGES.INCREDIBLE} ${score}.`;
+      break;
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      score = 40;
+      message = `${SCORE_MESSAGES.GREAT_JOB} ${score}.`;
+      break;
+    case 6:
+    case 7:
+    case 8:
+      score = 20;
+      message = `${SCORE_MESSAGES.GOOD_JOB} ${score}.`;
+      break;
+    case 9:
+    case 10:
+      score = 10;
+      message = `${SCORE_MESSAGES.TOOK_SEVERAL_ATTEMPTS} ${score}.`;
+      break;
+    default:
+      score = 0;
+      message = `${SCORE_MESSAGES.BETTER_LUCK_NEXT_TIME} ${score}.`;
+      break;
+  }
+
+  alert(message);
+  return score;
 };
 
 const game = () => {
-  const correctNumber = generateRandomNumber(MIN_NUMBER, MAX_NUMBER);
   let shouldRestart = false;
-  console.log("guess", correctNumber);
+  const correctNumber = generateRandomNumber(GAME_SETTINGS.MIN_NUMBER, GAME_SETTINGS.MAX_NUMBER);
 
   //Welcome Message(only for the first round)
-  !shouldRestart && alert(WELCOME_MESSAGE);
+  !shouldRestart && alert(GAME_FLOW_MESSAGES.WELCOME);
 
   // Play the round
-  playGameRound(correctNumber);
+  const numberOfAttempts = playGameRound(correctNumber);
+
+  //Calculate the score after the round
+  calculateScore(numberOfAttempts);
 
   //restarting the game
-  shouldRestart = confirm(TRY_AGAIN_MESSAGE);
-  shouldRestart ? game() : alert(THANKS_FOR_PLAYING_MESSAGE);
+  shouldRestart = confirm(PROMPTS_MESSAGES.TRY_AGAIN);
+  shouldRestart ? game() : alert(GAME_FLOW_MESSAGES.THANKS_FOR_PLAYING);
 };
 
 // play
