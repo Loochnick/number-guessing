3bd4222ba3e4a1998eb0865b8342ba1cd568e2c7
DESKTOP-A193QHJ\Asus <aminglbayatpour3@gmail.com>
2024-10-01 11:27:03 +0200

    improved code quality and added more feedbacks to the player and also persisted the palyer's data

--- utils.js
+++ utils.js
@@ -1,13 +1,30 @@
-import { GAME_SETTINGS, ERROR_MESSAGES } from "./constants.js";
+import {
+  GAME_SETTINGS,
+  ERROR_MESSAGES,
+  LOCAL_STORAGE_KEY,
+} from "./constants.js";
+
+import { DEFAULT_STATE } from "./gameState.js";
+
+//Local Storage helper functions
+export const saveStateToLocalStorage = (state) => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
+};
+
+export const loadStateFromLocalStorage = () => {
+  const savedState = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  if (savedState) return JSON.parse(savedState);
+  else return JSON.parse(JSON.stringify(DEFAULT_STATE)); //Deep Copy
+};
 
 export const validatePlayerGuess = (playerGuess) => {
-
-  if(playerGuess === "") {
+  if (playerGuess === "") {
     return ERROR_MESSAGES.NOT_EMPTY_SPACE;
   }
 
   const playerGuessNumber = Number(playerGuess);
-  
+
   if (isNaN(playerGuessNumber)) {
     return ERROR_MESSAGES.NOT_A_NUMBER;
   }
@@ -18,11 +35,34 @@ export const validatePlayerGuess = (playerGuess) => {
     return ERROR_MESSAGES.OUT_OF_RANGE_LOW;
   }
   if (playerGuessNumber > GAME_SETTINGS.MAX_NUMBER) {
-    return ERROR_MESSAGES.OUT_OF_RANGE_HIGH; 
+    return ERROR_MESSAGES.OUT_OF_RANGE_HIGH;
   }
 
   // Return null if valid
-  return null; 
+  return null;
+};
+
+// Function to calculate the total score and round statistics
+export const calculateGameStats = (state) => {
+  let totalScore = 0;
+  let roundsWon = 0;
+  let roundsLost = 0;
+  let roundsCancelled = 0;
+
+  state.rounds.forEach((round) => {
+    totalScore += round.score;
+
+    if (round.hasWon) roundsWon++;
+    else if (round.isCancelled) roundsCancelled++;
+    else roundsLost++;
+  });
+
+  return {
+    totalScore,
+    roundsWon,
+    roundsLost,
+    roundsCancelled,
+  };
 };
 
 export const generateRandomNumber = (min, max) =>
